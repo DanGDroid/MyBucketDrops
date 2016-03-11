421f97d8f0e09a0ef817b08619351545b7c9bfe5
daniel <danielgivoni@gmail.com>
2016-03-11 16:58:43 +0200

    lesson 9

--- app/src/main/java/com/example/admin/mybucketdrops/ActivityMain.java
+++ app/src/main/java/com/example/admin/mybucketdrops/ActivityMain.java
@@ -1,13 +1,40 @@
 package com.example.admin.mybucketdrops;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ActivityMain extends AppCompatActivity {
+import com.bumptech.glide.Glide;
 
+public class ActivityMain extends AppCompatActivity implements View.OnClickListener {
+
+    Toolbar mToolbar;
+    Button mbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mbutton = (Button) findViewById(R.id.btn_add);
+        mbutton.setOnClickListener(this);
+        setSupportActionBar(mToolbar);
+        initBackgroundImage();
+    }
+
+    private void initBackgroundImage(){
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(background);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(ActivityMain.this, "Adding a drop", Toast.LENGTH_SHORT).show();
     }
 }
